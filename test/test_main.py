9724b52d1e454c25207cf011c3fd47d045627dbf
a-chacon <andres.ch@protonmail.com>
2022-03-24 16:01:58 -0300

    fix: remove structure

--- test/test_main.py
+++ test/test_main.py
@@ -1,9 +1,3 @@
-from fastapi.testclient import TestClient
-
-from test.test import test_app
-
-client = TestClient(test_app())
-
-def test_main():
+def test_main(client):
     response = client.get("/")
     assert response.status_code == 200
