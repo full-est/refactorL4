5ae308434b292b63fb540a838397e4f280573a11
a-chacon <andres.ch@protonmail.com>
2022-03-29 15:13:26 -0300

    fix: 3600 to expiration minutes for jwt

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -19,7 +19,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
             headers={"WWW-Authenticate": "Bearer"},
         )
     # generate token
-    access_token_expires = timedelta(minutes=30)
+    access_token_expires = timedelta(minutes=3600)
     access_token = jwt.create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
     )
